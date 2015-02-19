0d28032f9bba86bf071e39830f98b263f40c04b9
Christoffer <christoffer@hush.ai>
2015-02-19 02:04:47 +0100

    Updated action bar, force landscape and full screen view
    I think the full screen change I made earlier got lost somehow. Found a
    better solution in this update. Reverted the action bar to the native
    design.

--- android/Circles/app/src/main/java/deltagruppen/circles/MainActivity.java
+++ android/Circles/app/src/main/java/deltagruppen/circles/MainActivity.java
@@ -1,9 +1,11 @@
 package deltagruppen.circles;
 
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.WindowManager;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -11,6 +13,8 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
     }
 

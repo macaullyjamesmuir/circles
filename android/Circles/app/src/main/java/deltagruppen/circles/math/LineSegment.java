6b96fd6dff7bfc5bc5286f165da4696d6031d3eb
Macaully James Muir <macaullyjames@gmail.com>
2015-03-02 23:12:12 +0100

    LineSegment now accepts PointF in the constructor.
    Previously you'd have to pass two Vector2Ds, so this makes working with user input a little less verbose and generally easier to work with. It's also nice to keep the math dependencies within the math package.

--- android/Circles/app/src/main/java/deltagruppen/circles/math/LineSegment.java
+++ android/Circles/app/src/main/java/deltagruppen/circles/math/LineSegment.java
@@ -1,5 +1,7 @@
 package deltagruppen.circles.math;
 
+import android.graphics.PointF;
+
 import org.apache.commons.math3.geometry.euclidean.twod.Vector2D;
 
 /**
@@ -8,6 +10,16 @@ import org.apache.commons.math3.geometry.euclidean.twod.Vector2D;
 public class LineSegment {
     public final Vector2D p1;
     public final Vector2D p2;
+
+    /**
+     * Create a new line segment with the given points.
+     * @param p1 A point.
+     * @param p2 A point.
+     */
+    public LineSegment(PointF p1, PointF p2)
+    {
+        this(new Vector2D(p1.x, p1.y), new Vector2D(p2.x, p2.y));
+    }
 
     /**
      * Create a new line segment with the given points.

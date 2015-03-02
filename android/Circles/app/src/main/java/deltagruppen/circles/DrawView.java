154f626879a487eae208c42e2f1ca99299faa516
Macaully James Muir <macaullyjames@gmail.com>
2015-03-02 19:39:29 +0100

    Moved to a Path-based drawing strategy.
    Instead of drawing directly to the canvas, a Path is built with straight lines. This is both cleaner and opens up for more andvanced interpolation later on.

--- android/Circles/app/src/main/java/deltagruppen/circles/DrawView.java
+++ android/Circles/app/src/main/java/deltagruppen/circles/DrawView.java
@@ -4,18 +4,19 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.graphics.PointF;
 import android.support.annotation.NonNull;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
 
-import java.util.Iterator;
 import java.util.LinkedList;
 
 public class DrawView extends View
 {
     private final LinkedList<PointF> points;
+    private final Path               path;
     private final Paint              paint;
 
     public DrawView(Context context, AttributeSet attrs)
@@ -23,6 +24,7 @@ public class DrawView extends View
         super(context, attrs);
         points = new LinkedList<>();
         paint  = new Paint();
+        path   = new Path();
         paint.setStyle(Paint.Style.STROKE);
         paint.setStrokeWidth(10);
         paint.setColor(Color.BLACK);
@@ -32,31 +34,27 @@ public class DrawView extends View
     protected void onDraw(Canvas canvas)
     {
         super.onDraw(canvas);
-        if (points.size() == 0) return;
-
-        Iterator<PointF> iterator = points.iterator();
-        PointF point = iterator.next();
-
-        while (iterator.hasNext()) {
-            PointF next = iterator.next();
-            canvas.drawLine(point.x, point.y, next.x, next.y, paint);
-            point = next;
-        }
+        canvas.drawPath(path, paint);
     }
 
     @Override
     public boolean onTouchEvent(@NonNull MotionEvent event) {
         if (event.getAction() == MotionEvent.ACTION_DOWN) {
-            points.clear();
-            points.add(new PointF(event.getX(), event.getY()));
             invalidate();
+
+            points.clear();
+            path.reset();
+            points.add(new PointF(event.getX(), event.getY()));
+            path.moveTo(event.getX(), event.getY());
             return true;
         }
         if (event.getAction() == MotionEvent.ACTION_MOVE) {
             for (int i = 0; i < event.getHistorySize(); i++) {
                 points.add(new PointF(event.getHistoricalX(i), event.getHistoricalY(i)));
+                path.lineTo(event.getHistoricalX(i), event.getHistoricalY(i));
             }
             points.add(new PointF(event.getX(), event.getY()));
+            path.lineTo(event.getX(), event.getY());
             invalidate();
             return true;
         }

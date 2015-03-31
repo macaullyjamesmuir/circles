0be78a2cc889e6a221885ef9480232ba6ca590dc
Dante <dante.tnc.johansson@gmail.com>
2015-03-31 21:23:35 +0200

    Revert "Revert "Revert "Added menu and made color changes"""
    This reverts commit 00463dc425a1159857dcf48a745b40abb8caacbb.

--- android/Circles/app/src/main/java/deltagruppen/circles/MainActivity.java
+++ android/Circles/app/src/main/java/deltagruppen/circles/MainActivity.java
@@ -59,11 +59,6 @@ public class MainActivity extends ActionBarActivity {
         startActivity(intent);
     }
 
-    public void showAppInfo(View view) {
-        Intent intent = new Intent(this, AppInfo.class);
-        startActivity(intent);
-    }
-
     public void showCalculatingPiInfo(MenuItem item) {
         Intent intent = new Intent(this, CalculatingPi.class);
         startActivity(intent);
